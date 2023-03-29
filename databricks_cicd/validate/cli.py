d87fb3e4f242ae49234f42b6a672035c0ac90b97
Manol Manolov <manol.manolov@gmail.com>
2023-03-29 10:00:19 -0400

    add content validations

--- databricks_cicd/validate/cli.py
+++ databricks_cicd/validate/cli.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from os import sep, path as op
 import logging
 import click
 import databricks_cicd.utils.helpers as helpers
@@ -47,21 +48,58 @@ def validate_cli(**kwargs):
     context = Context(conf)
 
     workspace = helpers.WorkspaceHelper(context)
-    if conf.workspace.deploy:
-        _log.info('Validating workspace...')
+    _log.info('Validating workspace...')
+    if conf.validate.workspace_python_notebook_header:
+        _log.info('  workspace_python_notebook_header')
         for i in workspace.local_items:
             local_item = workspace.local_items[i]  # type: Item
-    #         TODO: check if references to other notebooks are valid
+            if local_item.language == 'PYTHON':
+                file_header = Local.head(local_item.path)
+                assert file_header == '# Databricks notebook source\n', \
+                    'Invalid Python notebook header "{}".'.format(file_header)
 
     instance_pools = helpers.InstancePoolsHelper(context)
-    if conf.instance_pools.deploy:
-        _log.info('Validating instance pools...')
-        for i in instance_pools.local_items:
-            local_item = instance_pools.local_items[i]  # type: Item
-            instance_pools.get_local(local_item)
-            name_attribute = 'instance_pool_name'
-            if local_item.content.get(name_attribute) != Local.get_file_name(local_item.path):
-                _log.error('%s')
-            print(local_item.content)
 
+    clusters = helpers.ClustersHelper(context, instance_pools)
+    _log.info('Validating clusters...')
+    if conf.validate.clusters_no_nested_folders:
+        _log.info('  clusters_no_nested_folders')
+        for i in clusters.local_items:
+            assert len(i.split(sep)) == 1, 'Cluster file {} is in a subfolder. ' \
+                'All cluster files should be in a single folder without nesting.'.format(clusters.local_items[i].path)
+    if conf.validate.clusters_name:
+        _log.info('  clusters_name')
+        for i in clusters.local_items:
+            local_item = clusters.local_items[i]  # type: Item
+            clusters.get_local(local_item, curate_item=False)
+            cluster_name = local_item.content['cluster_name']
+            assert len(cluster_name) > 0, 'File {0} has no cluster_name.'.format(local_item.path)
+            assert op.splitext(op.basename(local_item.path))[0] == cluster_name, \
+                'File {} has different name than the cluster "{}" defined in it.'.format(
+                    local_item.path, cluster_name)
+
+    jobs = helpers.JobsHelper(context, clusters, workspace)
+    _log.info('Validating jobs...')
+    if conf.validate.jobs_no_nested_folders:
+        _log.info('  jobs_no_nested_folders')
+        for i in jobs.local_items:
+            assert len(i.split(sep)) == 1, 'Job file {} is in a subfolder. ' \
+                'All job files should be in a single folder without nesting.'.format(jobs.local_items[i].path)
+    if conf.validate.jobs_name:
+        _log.info('  jobs_name')
+        for i in jobs.local_items:
+            local_item = jobs.local_items[i]  # type: Item
+            jobs.get_local(local_item, curate_item=False)
+            job_name = local_item.content.get('name', '')
+            assert len(job_name) > 0, 'File {0} has no job name.'.format(local_item.path)
+            assert op.splitext(op.basename(local_item.path))[0] == job_name, \
+                'File {} has different name than the job "{}" defined in it.'.format(local_item.path, job_name)
+    if conf.validate.jobs_existing_clusters:
+        _log.info('  jobs_existing_clusters')
+        for i in jobs.local_items:
+            jobs.validate_existing_cluster_name(jobs.local_items[i])
+    if conf.validate.jobs_notebook_paths:
+        _log.info('  jobs_notebook_paths')
+        for i in jobs.local_items:
+            jobs.validate_notebook_path(jobs.local_items[i])
     _log.info('All done!')
